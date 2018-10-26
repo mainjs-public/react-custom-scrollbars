0f3a3660bf808e87ec69987ec5695c74456947b2
Dang Tien Ngoc <dangtienngoc@hotmail.com>
2018-10-26 08:15:35 +0700

    Move folder

---
+++ example/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import Scroll from '../../src/Scroll';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Scroll />
+      </div>
+    );
+  }
+}
+
+export default App;
